7dcf07bf50127a83ca4101cf06ed8d9dac735723
erickgonzalezjs <gonzalezsolanoerick974@gmail.com>
2023-10-06 18:57:34 -0500

    Readme file change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # integrativeproject
+# erick gonzalez
--- integrative_project-main/Proyecto001/js/core/code.js
+++ integrative_project-main/Proyecto001/js/core/code.js
@@ -1,5 +1,7 @@
 function AddItem(val) {
-    // document.getElementById("salesItems").innerHTML = AddTableItem(1, val, `other: ${val}`, "1234");
+  // document.getElementById("salesItems").innerHTML = AddTableItem(1, val, `other: ${val}`, "1234");
 
-    document.getElementById("salesItems").appendChild(AddTableItemOption2(1, val, `other: ${val}`, "1234"));
+  document
+    .getElementById("salesItems")
+    .appendChild(AddTableItemOption2(1, val, `other: ${val}`, "1234"));
 }
